60319c9fbafcfc267c34f780117c5a48950e8107
splayyyyyuuuxauuax <42817210+splayyyyyuuuxauuax@users.noreply.github.com>
2018-09-01 23:22:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,12 +2,26 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 
-client.on('guildMemberAdd' ,member => {
-  if(member.guild.id !== '408926307372498945') return;
-  setTimeout(function(){
-  member.guild.channels.find(r => r.id === '485545597877747713').send('Welcome To **Bُubbles** Tumblr .');
-},3000);
-});
+var PrEfix = "p!";
+client.on('message', message => {
+  if (!message.content.startsWith(PrEfix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== 'ur id') return;
+if (message.content.startsWith(PrEfix + 'wt')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
 
+if (message.content.startsWith(PrEfix + 'ls')) {
+client.user.setActivity(argresult, {type:'LISTENING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+if (message.content.startsWith(PrEfix + 'st')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/Baktash_183");
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+}
+
+});
 
 client.login(process.env.BOT_TOKEN);
