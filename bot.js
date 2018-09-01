61c4fd8080fe17a1da0dca1e017e12246917703f
splayyyyyuuuxauuax <42817210+splayyyyyuuuxauuax@users.noreply.github.com>
2018-09-01 23:35:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 
-var PrEfix = "p!";
+var PrEfix = "-";
 client.on('message', message => {
   if (!message.content.startsWith(PrEfix)) return;
   var args = message.content.split(' ').slice(1);
