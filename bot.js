29ce2749ccbfc7e80d2cf1560dff43a434384e64
splayyyyyuuuxauuax <42817210+splayyyyyuuuxauuax@users.noreply.github.com>
2018-09-04 15:54:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,32 +1,30 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
+var prefix = "c!";
+client.on("message", message => {
 
-console.log("Welcome Again !");
- 
- client.on("guildMemberRemove", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(` 
-**
-هلآ والله بك ي كيكة تجي تنورني بسرفري ونكون آصحآب ي قلبي م يفرق شي لو بتجي لعندنآ ي عمري خلنآ نتعآرف  خسآرة والله فخم زيك م يكون عندنآ
-https://discord.gg/c95c8zF 
-:hearts:
-الدعووة خاصة لك ي قلبي ... [ ${member}  ]
-**`)
-}).catch(console.error)
-
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
 })
- 
-client.on("guildMemberAdd", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(` 
-**
-هلآ والله بك ي كيكة تجي تنورني بسرفري ونكون آصحآب ي قلبي م يفرق شي لو بتجي لعندنآ ي عمري خلنآ نتعآرف  خسآرة والله فخم زيك م يكون عندنآ
-https://discord.gg/c95c8zF 
-:hearts:
-الدعووة خاصة لك ي قلبي ... [ ${member}  ]
-**`) 
-}).catch(console.error)
-})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\`** :white_check_mark: جآري آرسال البرودكآست ل  **`); 
+ message.delete(); 
+};     
+});
 
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`AG CLAN BOT- Script By : PREDATOR`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : PREDATOR ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Cٌoffee D3M.`,"https://www.twitch.tv/predator12_")
+client.user.setStatus("dnd")
+});
 
 client.login(process.env.BOT_TOKEN);
